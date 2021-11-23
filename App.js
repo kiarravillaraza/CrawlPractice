3855165428779fa554e9bec55333c3fdf5d5465c
Kiarra Villaraza <kvillaraza@luc.edu>
2021-11-23 17:13:14 -0600

    first commit

---
+++ App.js
@@ -0,0 +1,81 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+
+ import React, { Component } from "react";
+ import { Platform, Image, ScrollView, StyleSheet, Text, View } from "react-native";
+ import {BackgroundCarousel} from './BackgroundCarousel'
+
+
+ const dime = [
+
+  "https://scontent-ort2-1.xx.fbcdn.net/v/t39.30808-6/p180x540/256827177_675290333857410_1923687880662733746_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=9267fe&_nc_ohc=5IGNaem70qMAX_fhCGW&_nc_oc=AQlfID9W65J_8qrh0U_q0TerTvi5u26Z9cER4btQCWseL-gwQaKqcgc6mZx4E8FDUko&_nc_ht=scontent-ort2-1.xx&oh=db2241bfed32b951a577e20e9e072b34&oe=61A1E7FB",
+   "https://scontent-ort2-1.xx.fbcdn.net/v/t1.6435-9/78566939_158829685503480_2064189259874566144_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=e3f864&_nc_ohc=SGMX0In75jAAX-Z2BOl&_nc_ht=scontent-ort2-1.xx&oh=fc51a9b7d411ede02af6349b199f4328&oe=61C38364",
+   "https://scontent-ort2-1.xx.fbcdn.net/v/t1.6435-9/201156058_572739830779128_5055751502644027532_n.jpg?_nc_cat=104&ccb=1-5&_nc_sid=2c4854&_nc_ohc=fvAoMqkJHekAX9oWPTo&_nc_ht=scontent-ort2-1.xx&oh=102a04e145e82d4fcd5bf2f42ac8a91f&oe=61C1CD88",
+   "https://scontent-ort2-1.xx.fbcdn.net/v/t39.30808-6/258091832_676527673733676_8826771448272167123_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=9267fe&_nc_ohc=mUlMsqEIeI8AX8cvVN8&_nc_ht=scontent-ort2-1.xx&oh=ac2ffabd58ec75c0e89ca562b499ad99&oe=61A06239",
+   "https://d1t295ks1d26ah.cloudfront.net/media/pictures/files/000/021/263/xlarge_desktop/77394087_158830698836712_6283762755795156992_o.jpg?1589578908"
+
+ ];
+ 
+
+ export default class App extends Component {
+   render() {
+     return (
+      
+     <View style={styles.container}>
+        <BackgroundCarousel images={dime} />
+        <Text style = {styles.welcome}>
+          The Dime 
+        </Text>
+    </View>
+     );
+   }
+ }
+ 
+ const styles = StyleSheet.create({
+   container: {
+     flex: 1,
+     justifyContent: "center",
+     alignItems: "center",
+     backgroundColor: "#023047"
+   },
+   welcome: {
+     fontSize: 35,
+     color: "#fff",
+     textAlign: "center",
+     bottom: 50,
+     fontWeight: "bold",
+     letterSpacing: 3.5,
+     margin: 10
+   },
+   instructions: {
+     textAlign: "center",
+     color: "#333333",
+     marginBottom: 5
+   }
+ });
+
+
+
+
+
+
+
+//  import React from 'react';
+//  import ImagesExample from './components/image_example'
+
+// const App = () => {
+//   return (
+//     <ImagesExample />
+//   )
+// }
+
+
+// export default App
+
+
---
+++ BackgroundCarousel.js
@@ -0,0 +1,131 @@
+import * as React from 'react';
+import { StyleSheet, View, Text, ScrollView, Dimensions, Image } from 'react-native';
+
+/*
+URL for IMAGE SLIDER CODE
+ https://www.youtube.com/watch?v=ReAmjdNyMJA 
+ */ 
+
+const DEVICE_WIDTH = Dimensions.get("window").width;
+
+class BackgroundCarousel extends React.Component {
+    scrollRef = React.createRef();
+    constructor(props) {
+
+        super(props);
+
+        this.state = {
+            selectedIndex: 0
+        }
+    }
+
+    componentDidMount = () => {
+        setInterval(() => {
+            
+        this.setState(prev => ({selectedIndex: prev.selectedIndex === this.props.images.length - 1 ? 0 : prev.selectedIndex + 1}), 
+        () => {
+
+            this.scrollRef.current.scrollTo({
+                animated: true,
+                y: 0,
+                x: DEVICE_WIDTH *  this.state.selectedIndex
+            })
+
+        })
+        
+        } , 3500)
+        
+    }
+    setSelectedIndex = event => {
+        // width of the viewSize 
+        const viewSize = event.nativeEvent.layoutMeasurement.width;
+        // get current position in scrollView
+        const contentOffset = event.nativeEvent.contentOffset.x;
+
+        const selectedIndex = Math.floor(contentOffset/viewSize);
+
+        this.setState({selectedIndex});
+    }
+
+
+
+
+    render() {
+        const {images} = this.props
+        const {selectedIndex} = this.state
+        return(
+
+        
+            < View style ={{position: "absolute", bottom: 506, height: "40%", width: "100%"}}>
+                    <ScrollView 
+                    horizontal 
+                    pagingEnabled 
+                    onMomentumScrollEnd={this.setSelectedIndex}
+                    ref={this.scrollRef}
+                    >
+                        {images.map(image => (
+                            <Image 
+                            key={image}
+                            source={{uri: image}}
+                            style={styles.backgroundImage}
+                            />
+
+                        
+                        ))}
+                    </ScrollView>
+                    
+                    
+                    <View style={styles.circleDiv}>
+                        {images.map((image, i) => (
+                            <View 
+                            key={image}   style={[styles.whiteCircle, {opacity: i == selectedIndex ? 0.5 : 1}]} />
+                        ))}
+                    </View>             
+                  
+
+           </View>
+      
+
+      
+
+        
+    
+
+        );
+    }
+
+
+}
+
+const styles = StyleSheet.create({
+    backgroundImage: {
+        height: "100%",
+        width: DEVICE_WIDTH
+        
+   },
+   circleDiv: {
+    position: "absolute",
+    bottom: 25,
+    height: 10,
+    width: "100%",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center"
+    },
+    whiteCircle: {
+        width: 6,
+        height: 6,
+        borderRadius: 3,
+        margin: 5, 
+        backgroundColor: "#fff"
+    },
+   
+    
+    
+   
+});
+
+
+export {BackgroundCarousel};
+
