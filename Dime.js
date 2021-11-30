f4123c1a7568ce9ad8e5909c3ee074e6fb8b64a3
-- <kvillaraza@luc.edu>
2021-11-30 15:50:56 -0600

    second commit -- singular Bar file created

---
+++ Dime.js
@@ -0,0 +1,45 @@
+import {StyleSheet, Text} from 'react-native';
+import React from 'react';
+
+const styles = StyleSheet.create({
+    barStyle: {
+    fontSize: 40,
+    color: "#fff",
+    textAlign: "center",
+    bottom: 50,
+    fontWeight: "bold",
+    fontFamily: "Futura",
+    letterSpacing: 3.5,
+    margin: 10
+    }
+});
+class Dime extends React.Component {
+
+    render () {
+        return (
+    <Text style = {styles.barStyle}>
+      The Dime
+      The Dime
+      The Dime
+      The Dime
+      The Dime
+      The Dime
+      The Dime
+      The Dime
+      The Dime
+      The Dime
+      The Dime
+      The Dime
+      The Dime
+      The Dime
+      The Dime
+      The Dime
+      The Dime
+      The Dime
+
+    </Text>
+        );
+}
+}
+
+export default Dime;
